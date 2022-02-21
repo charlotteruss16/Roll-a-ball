d65e5f9ccd9acebaac99dbb838e9df2e23d64a0a
Charlotte Russell <crussell2@myjmc.edu.au>
2022-02-21 11:53:19 +1100

    Week 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
     public float speed = 5;
+    public int score;
+    int PickupCont;
+    Timer timer;
 
     void Start()
     {
+        score = 0;
         rb = GetComponent<Rigidbody>();
+        PickupCont = GameObject.FindGameObjectsWithTag("Pickup").Length;
+        timer = FindObjectOfType<Timer>();
+        timer.StartTimer();
     }
 
-    // Update is called once per frame
+
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -20,5 +27,25 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
         rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pickup"))
+        {
+            Destroy(other.gameObject);
+            score++;
+            if (score == PickupCont)
+            {
+                WinGame();
+            }
+        }
+    }
+
+    void WinGame()
+    {
+        timer.StopTimer();
+        print("YOU WIN!!!! YAY!!!! Your time was: " + timer.GetTime().ToString("F2"));
+ 
     }
 }
